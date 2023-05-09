5dfce5a24811f8b15ac3adfac77ddb50bb3213d0
ivgordeev <ivangordeev110@gmail.com>
2023-05-10 01:54:19 +0300

    Add account activation

--- src/main/java/com/tweetclone/controller/RegistrationController.java
+++ src/main/java/com/tweetclone/controller/RegistrationController.java
@@ -1,16 +1,14 @@
 package com.tweetclone.controller;
 
 
-import com.tweetclone.entity.Role;
 import com.tweetclone.entity.User;
-import com.tweetclone.repository.UserRepository;
+import com.tweetclone.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-
-import java.util.Collections;
 
 /**
  * @author Ivan Gordeev 07.05.2023
@@ -18,7 +16,7 @@ import java.util.Collections;
 @Controller
 public class RegistrationController {
     @Autowired
-    private UserRepository userRepository;
+    private UserService userService;
 
     @GetMapping("/registration")
     public String registration() {
@@ -27,14 +25,21 @@ public class RegistrationController {
 
     @PostMapping("/registration")
     public String doRegister(User user, Model model) {
-        User userFromDb = userRepository.findByUsername(user.getUsername());
-        if (userFromDb != null) {
+        if (!userService.addUser(user)) {
             model.addAttribute("message", "User is already exists!");
             return "registration";
         }
-        user.setActive(true);
-        user.setRoles(Collections.singleton(Role.USER));
-        userRepository.save(user);
         return "redirect:/login";
+    }
+
+    @GetMapping("/activate/{code}")
+    private String activate(Model model, @PathVariable String code) {
+        boolean isActive = userService.activeUser(code);
+        if (isActive) {
+            model.addAttribute("message", "User successfully activated");
+        } else {
+            model.addAttribute("message", "Activation code not found");
+        }
+        return "login";
     }
 }
--- src/main/java/com/tweetclone/service/UserService.java
+++ src/main/java/com/tweetclone/service/UserService.java
@@ -1,11 +1,17 @@
 package com.tweetclone.service;
 
+import com.tweetclone.entity.Role;
+import com.tweetclone.entity.User;
 import com.tweetclone.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+
+import java.util.Collections;
+import java.util.UUID;
 
 /**
  * @author Ivan Gordeev 08.05.2023
@@ -15,8 +21,41 @@ public class UserService implements UserDetailsService {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private MailSender mailSender;
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         return userRepository.findByUsername(username);
+    }
+
+    public boolean addUser(User user) {
+        User userFromDb = userRepository.findByUsername(user.getUsername());
+        if (userFromDb != null) {
+            return false;
+        }
+        user.setActive(false);
+        user.setRoles(Collections.singleton(Role.USER));
+        user.setActivationCode(UUID.randomUUID().toString());
+        userRepository.save(user);
+        if (StringUtils.hasLength(user.getEmail())) {
+            String message = String.format("Hello, %s!\n" +
+                    "Welcome to TweetClone.\n" +
+                    "To activate your account, please, visit next link: http://localhost:8080/activate/%s",
+                    user.getUsername(), user.getActivationCode());
+            mailSender.send(user.getEmail(), "Activation code", message);
+        }
+        return true;
+    }
+
+    public boolean activeUser(String code) {
+        User user = userRepository.findByActivationCode(code);
+        if (user == null) {
+            return false;
+        }
+        user.setActivationCode(null);
+        user.setActive(true);
+        userRepository.save(user);
+        return true;
     }
 }
