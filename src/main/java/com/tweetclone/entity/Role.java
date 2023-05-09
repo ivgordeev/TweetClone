d055af6f8d2fe5704c67d49a015003f9b3546bfc
ivgordeev <ivangordeev110@gmail.com>
2023-05-09 16:48:20 +0300

    Add admin role and check rights

--- src/main/java/com/tweetclone/entity/Role.java
+++ src/main/java/com/tweetclone/entity/Role.java
@@ -3,7 +3,8 @@ package com.tweetclone.entity;
 import org.springframework.security.core.GrantedAuthority;
 
 public enum Role implements GrantedAuthority {
-    USER;
+    USER,
+    ADMIN;
 
     @Override
     public String getAuthority() {
